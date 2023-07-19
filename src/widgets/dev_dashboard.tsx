3017a2b8e451af289e4a07e86962b5607d6452d5
Hannes Frank <frankhannes@gmail.com>
2023-07-19 09:23:23 +0200

    Add Dev Dashboard widget
    The dashboard can visualise SDK methods reactively.

---
+++ src/widgets/dev_dashboard.tsx
@@ -0,0 +1,99 @@
+import { usePlugin, renderWidget, useTracker, RNPlugin, Rem } from '@remnote/plugin-sdk';
+import { format } from 'path';
+import { ReactNode, createContext, useContext } from 'react';
+
+export const DevDashboard = () => {
+  const plugin = usePlugin();
+  return (
+    <>
+      <APINamespace name="app">
+        <APIMethod method="getOperatingSystem" />
+        <APIMethod method="getPlatform" />
+      </APINamespace>
+
+      <APINamespace name="focus">
+        <APIMethod method="getFocusedRem" />
+      </APINamespace>
+      <APINamespace name="editor">
+        <APIMethod method="getFocusedEditorText" />
+        <APIMethod method="getSelection" />
+      </APINamespace>
+    </>
+  );
+};
+
+const APINamespace = ({ name, children }: { name: keyof RNPlugin; children: ReactNode }) => (
+  <APINamespaceContext.Provider value={name}>
+    <div className="m-2">
+      <h2 className="text-lg mb-2 font-mono font-bold">{name}</h2>
+      {children}
+    </div>
+  </APINamespaceContext.Provider>
+);
+
+const APINamespaceContext = createContext<keyof RNPlugin>('editor');
+
+const APIMethod = (
+  props: { method: string; value?: (plugin: RNPlugin) => unknown },
+  doc?: ReactNode | string
+) => {
+  const namespace = useContext(APINamespaceContext);
+  const defaultValueFunc = (plugin: RNPlugin) => {
+    const n = plugin[namespace];
+    // @ts-ignore
+    return n ? n[props.method]() : null;
+  };
+  const methodResult = useTracker(props.value || defaultValueFunc);
+
+  return (
+    <div className="my-1">
+      <span className="text-sm font-normal font-mono">
+        <DocLink namespace={namespace} method={props.method}>
+          {props.method}
+        </DocLink>
+        :
+      </span>{' '}
+      <span className="text-xs">{formatResult(methodResult)}</span>
+    </div>
+  );
+};
+
+const DocLink = (props: { namespace: string; method: string; children: ReactNode }) => {
+  const namespaceName = props.namespace.charAt(0).toUpperCase() + props.namespace.slice(1);
+  return (
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href={`https://plugins.remnote.com/api/classes/${namespaceName}Namespace#${props.method.toLowerCase()}`}
+    >
+      {props.children}
+    </a>
+  );
+};
+
+function isRem(obj: any) {
+  // For some reason initially `instanceof Rem` was always false.
+  // obj?.__proto__?.constructor?.name === 'Rem'
+  // return obj && obj.__proto__ === Rem.prototype;
+  return obj instanceof Rem;
+}
+
+function formatResult(result: unknown) {
+  if (typeof result === 'string') {
+    return result;
+  }
+  if (typeof result === 'undefined') {
+    return <em>undefined</em>;
+  }
+  if (isRem(result)) {
+    return `Rem(${JSON.stringify({
+      // @ts-ignore
+      id: result?._id,
+      // @ts-ignore
+      text: result?.text,
+    })})`;
+  }
+  return JSON.stringify(result);
+}
+
+renderWidget(DevDashboard);
--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -4,42 +4,47 @@ import '../App.css';
 import { writeFileSync } from 'fs';
 
 async function onActivate(plugin: ReactRNPlugin) {
-  // Register settings
-  await plugin.settings.registerStringSetting({
-    id: 'name',
-    title: 'What is your Name?',
-    defaultValue: 'Bob',
-  });
-
-  await plugin.settings.registerBooleanSetting({
-    id: 'pizza',
-    title: 'Do you like pizza?',
-    defaultValue: true,
-  });
-
-  await plugin.settings.registerNumberSetting({
-    id: 'favorite-number',
-    title: 'What is your favorite number?',
-    defaultValue: 42,
-  });
-
   // A command that inserts text into the editor if focused.
   await plugin.app.registerCommand({
     id: 'editor-command',
     name: 'Write File',
     action: async () => {
-      await writeFileSync('/Users/hannesfrank/fromRemNote', "Hello from remnote");
+      // await writeFileSync('/Users/hannesfrank/fromRemNote', "Hello from remnote");
       plugin.editor.insertPlainText('Hello World!');
     },
   });
 
-  // Show a toast notification to the user.
-  await plugin.app.toast("I'm a toast!");
+  await plugin.app.registerCommand({
+    id: 'editor-command3',
+    name: 'Write File',
+    action: async () => {
+      // await writeFileSync('/Users/hannesfrank/fromRemNote', "Hello from remnote");
+      plugin.editor.insertPlainText('Hello World!');
+    },
+  });
+
+  await plugin.app.registerWidget('dev_dashboard', WidgetLocation.Pane, {});
+
+
+  await plugin.app.registerCommand({
+    id: 'toggle-dev-pane',
+    name: 'Show Dev Dashboard',
+    action: async () => {
+      plugin.window.openWidgetInPane('dev_dashboard');
+    },
+  });
+  // await plugin.app.registerCommand({
+  //   id: 'toggle-dev-pane',
+  //   name: 'Show Dev Dashboard (Floating)',
+  //   action: async () => {
+  //     plugin.window.openFloatingWidget('dev_dashboard', {});
+  //   },
+  // });
 
   // Register a sidebar widget.
-  await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
-    dimensions: { height: 'auto', width: '100%' },
-  });
+  // await plugin.app.registerWidget('sample_widget', WidgetLocation.RightSidebar, {
+  //   dimensions: { height: 'auto', width: '100%' },
+  // });
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
