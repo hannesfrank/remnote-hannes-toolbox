e58ca48e99990e122ea7f57d2f68fb252cc27a4e
Hannes Frank <frankhannes@gmail.com>
2023-08-09 10:57:07 +0200

    Add basic RemNote button
    Just to have something for now.
    There should be a package with simple versions of all
    RemNote core components.

---
+++ src/components/builtin/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// TODO: Button kinds
+// TODO: Package other RemNote core components
+function Button() {
+  return (
+    <button className="rn-button button box-border inline-flex select-none flex-row items-center justify-center text-center cursor-pointer relative transition-all rounded-md rn-button--Outline py-1.5 px-3 h-8 rn-text-label-medium hover:bg-gray-10 rn-clr-background-primary text-gray-100 border border-solid border-gray-15"></button>
+  );
+}
+
+export default Button;
