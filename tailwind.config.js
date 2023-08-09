0d463a749c234b309d96ee9f2fbc937517033364
Hannes Frank <frankhannes@gmail.com>
2023-08-09 11:33:49 +0200

    Tailwind auto columns
    Not used at the moment, maybe later

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    gridTemplateColumns: {
+      'auto-fill-6': 'repeat(auto-fill, minmax(24px, 1fr))',
+      'auto-fill-12': 'repeat(auto-fill, minmax(48px, 1fr))',
+      'auto-fill-24': 'repeat(auto-fill, minmax(96px, 1fr))',
+      'auto-fill-36': 'repeat(auto-fill, minmax(144px, 1fr))',
+      'auto-fill-48': 'repeat(auto-fill, minmax(192px, 1fr))',
+      'auto-fill-72': 'repeat(auto-fill, minmax(288px, 1fr))',
+    },
     extend: {},
   },
   plugins: [],
